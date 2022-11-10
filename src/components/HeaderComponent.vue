8627c282d566b84f2cd11661e7602727bfb93d7c
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-11-10 15:32:25 +0100

    Created the content of header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,121 @@
 <template>
     <header>
-        Questo è l'HEADER
+        <div class="container">
+            <div class="logo">
+                <a href="#">
+                    <img src="../assets/img/dc-logo.png" alt="Logo DC">
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="(link, index) in links" :key="index">
+                        <a :href="link.url" :class="{ 'active': link.current }">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
 <script>
     export default {
-        name: 'HeaderComponent'
+        name: 'HeaderComponent',
+        data() {
+            return {
+                links: [
+                    {
+                        text: "Characters",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "Comics",
+                        url: "#nogo",
+                        current: true,
+                    },
+                    {
+                        text: "Movies",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "Tv",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Games",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "Collectibles",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "Videos",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "Fans",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "News",
+                        url: "#nogo",
+                        current: false,
+                    },
+                    {
+                        text: "Shop",
+                        url: "#nogo",
+                        current: false,
+                    },
+                ],
+            };  
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/style/partials/mixins' as *;
+@use '../assets/style/partials/variables' as *;
 
+header {
+    
+    .container {
+        @include dflex;
+        
+        ul {
+                list-style: none;
+                @include dflex;
+
+                li {
+                    font-weight: bold;
+                    display: inline-block;
+                    text-transform: uppercase;
+                    margin-right: 20px;
+                    font-size: 0.8rem;
+
+                    a {
+                        text-decoration: none;
+                        color: $blacktext;
+                        height: 50px;
+                        padding-bottom: 50px;
+                    
+
+                        &.active,
+                        &:hover {
+                            color: $bluetext;
+                            border-bottom: 5px solid $bluetext;
+                        }
+                    }
+                }
+            }
+    }
+
+    
+}
 </style>
